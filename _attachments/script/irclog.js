1e78480fa6c71ea879b578ca4e2650bed3c6e1e1
Damjan Georgievski <gdamjan@gmail.com>
2011-10-17 21:01:43 +0200

    A big rewrite, the logic is still the same though

--- _attachments/script/irclog.js
+++ _attachments/script/irclog.js
@@ -4,30 +4,34 @@ jQuery(function ($) {
    var current_channel = $Utils.getQueryVariable("channel", "lugola");
    $('title').text('#' + current_channel + ' - IRClogger 2.0');
 
-   var next_startkey;
-   var previousDate = 0;
-   var focused = false;
+   var pagination = {begin: [current_channel, {}], end: [current_channel, 0] };
 
+   var focused = false;
    $(window).focus(function () { focused = true; });
    $(window).blur(function () { focused = false; });
-   $("#morehistory").click(loadMore);
-   $("#pi").click(showChannelList);
+
+   $('#prev_page').click(loadPrevPage);
+   $('#next_page').click(loadNextPage);
+   $('#settings a').click(function() {
+      TINY.box.show({url:this.href});
+      return false;
+   });
 
    var table = $("#irclog");
+   var cachedTBodySegments = {};
 
-   function fmtTimestamp(T) {
+   function timestampToDatetime(timestamp) {
       function pad(x)
          {return x < 10 ? '0' + x.toString() : x.toString()}
 
-      var t = new Date(T * 1000);
+      var dt = new Date(timestamp * 1000);
       return {
-         dt: pad(t.getFullYear()) + '-' + pad(t.getMonth() + 1) + '-' + pad(t.getDate()),
-         tm: pad(t.getHours()) + ':' + pad(t.getMinutes()) + ':' + pad(t.getSeconds())
+         date: dt.getFullYear() + '-' + pad(dt.getMonth() + 1) + '-' + pad(dt.getDate()),
+         time: pad(dt.getHours()) + ':' + pad(dt.getMinutes()) + ':' + pad(dt.getSeconds())
       }
    }
 
-   function fmtMessage(doc) {
-      var color = $Colorizer(doc.sender);
+   function fmtMessage(doc, color) {
       var out = '';
       out += '<span class="nickname" style="background-color:' + color + '">' + doc.sender + '</span>';
       msg = $Utils.escapeHTML(doc.message);
@@ -37,93 +41,120 @@ jQuery(function ($) {
       return out;
    }
 
-   function fmtHtmlDoc(doc) {
-      var t = fmtTimestamp(doc.timestamp);
-      var permalink = '?channel=' + doc.channel + ';date=' + t.dt + '#' + t.tm;
+   function makeTableRow(message, permalink, anchor, datetime) {
       var out = '';
-      // fucking ugly!!! think about a nicer way to leave the separators
-      if (previousDate != t.dt) {
-         out += '<tr><td class="date" colspan="2"><span>' + t.dt + '</span></td></tr>\n';
-         previousDate = t.dt;
-      }
       out += '<tr>';
       out += '<td valign="top" class="timestamp" width="1%">';
-      out += '<a id="' + t.tm + '" href="' + permalink + '">' + t.tm + '</a>';
+      out += '<a id="' + anchor + '" href="' + permalink + '">' + datetime.time + '</a>';
       out += '</td>';
-      out += '<td class="message">' + fmtMessage(doc) + '</td>';
+      out += '<td class="message">' + message + '</td>';
       out += '</tr>\n';
       return out;
    }
 
-   // callback, called when new data arives from the _changes notification
-   function cb_NewData(data) {
-      for (var k = 0; k < data.results.length; k++) {
-         var doc = data.results[k].doc;
-         var html = fmtHtmlDoc(doc);
-
-         if (!focused) {
-            $TitleAlert.start();
-         } else if (document.documentElement.scrollHeight - 100 < document.documentElement.clientHeight + window.pageYOffset) {
-            window.scroll(window.scrollX, document.body.clientHeight);
+   function displayRows(rows, descending) {
+      var i, len = rows.length;
+      for (i=0; i<len; i++) {
+         var doc = rows[i].doc;
+         var color = $Colorizer(doc.sender);
+         var datetime = timestampToDatetime(doc.timestamp);
+         var permalink = '?channel=' + doc.channel +
+                   ';date=' + datetime.date + '#' + datetime.time;
+         var anchor = datetime.date + '-' + datetime.time;  // FIXME ???
+         var msg = fmtMessage(doc, color);
+         var row = makeTableRow(msg, permalink, anchor, datetime);
+         var body = getTableSegment(datetime.date);
+         if (descending) {
+            $(body.children()[0]).after(row);
          } else {
-            $.noticeAdd({text: fmtMessage(doc)});
-         }
-         table.append(html);
-      }
-   }
-
-
-   function showChannelList() {
-      $Couch.view("channel", {
-         group: true,
-         group_level: 1
-      }).done( function (data) {
-         var div = $('<div></div>');
-         var list = $('<ul class="channels"></ul>');
-         for (var k = 0; k < data.rows.length; k++) {
-            var key = data.rows[k].key[0];
-            var value = data.rows[k].value;
-            list.append('<li><a title="' + value + '" href="?channel=' + key + '">' + key + '</a></li>');
-         }
-         div.append(list);
-         div.dialog({
-            modal: true,
-            height: 240,
-            title: "Channels logged"
-         });
-      }
-      );
-   }
-
-
-   function cb_GotLast100(data) {
-      var html = '';
-      for (var k = data.rows.length - 2; k >= 0; --k) {
-         var doc = data.rows[k].doc;
-         html += fmtHtmlDoc(doc);
-      }
-      table.prepend(html);
-      next_startkey = data.rows[data.rows.length - 1].key;
-   }
-
-
-   function cb_GotHistory(data) {
-      var html = '';
-      for (var k = data.rows.length - 1; k >= 0; --k) {
-         var doc = data.rows[k].doc;
-         html += fmtHtmlDoc(doc);
-      }
-      table.append(html);
-      var id = window.location.hash.substr(1);
-      if (id) {
-         var el = document.getElementById(id);
-         if (el) {
-            el.focus();
-            el.scrollIntoView(true);
+            body.append(row);
          }
       }
    }
 
+   function getTableSegment(date) {
+      var tbody = cachedTBodySegments[date];
+      if (tbody !== undefined)
+         return tbody;
+      tbody = $('<tbody>');
+      tbody.attr('id', date);
+      cachedTBodySegments[date] = tbody;
+      tbody.append('<tr><th class="date" colspan="2"><span>' + date + '</span></th></tr>');
+
+      var bodies = table.children('tbody');
+      var i, done = false;
+      for (i=0; i<bodies.length; i++) {
+         if (date < bodies[i].id) {
+            $(bodies[i]).before(tbody);
+            done = true;
+            break;
+         }
+      }
+      if (!done) table.append(tbody);
+
+      return tbody;
+   }
+
+
+   function loadNextPage() {
+      var v = $Couch.view("channel", {
+         startkey: pagination.end,
+         endkey: [current_channel, {}],
+         include_docs: true,
+         limit: 100,
+         descending: false
+      }).done(function (data) {
+         pagination.end = data.rows[data.rows.length - 1].key;
+         displayRows(data.rows);
+      });
+      return v;
+   }
+
+
+   function loadPrevPage() {
+      var v = $Couch.view("channel", {
+         startkey: pagination.begin,
+         endkey: [current_channel, 0],
+         include_docs: true,
+         limit: 100,
+         descending: true
+      }).done(function (data) {
+         pagination.begin = data.rows[data.rows.length - 1].key;
+         displayRows(data.rows, true);
+      });
+      return v;
+   }
+
+
+   function loadFullDay(date) {
+      var start = new Date(date).getTime() / 1000;
+      var end = start + 24 * 60 * 60;
+      var v = $Couch.view("channel", {
+         startkey: [current_channel, start],
+         endkey: [current_channel, end],
+         include_docs: true,
+         descending: false
+      }).done(function (data) {
+         pagination.begin = data.rows[0].key;
+         pagination.end = data.rows[data.rows.length - 1].key;
+         displayRows(data.rows);
+      });
+      return v;
+   }
+
+   // callback, called when new data arrives from the _changes notification feed
+   function on_update(data) {
+      displayRows(data.results);
+      if (!focused) {
+         $TitleAlert.start();
+      } else if (document.documentElement.scrollHeight - 100 <
+            document.documentElement.clientHeight + window.pageYOffset) {
+         // view is at the bottom of the document, scroll down to the end
+         window.scroll(window.scrollX, document.body.clientHeight);
+      } else {
+         $.noticeAdd({text: fmtMessage(doc)});
+      }
+   }
 
    function startUpdates(last_update_seq) {
       var query = {
@@ -132,45 +163,24 @@ jQuery(function ($) {
          channel: current_channel
       }
       var ch = $Couch.changes(last_update_seq, query);
-      ch.on_change(cb_NewData);
-      ch.on_error(console.log);
+      ch.on_change(on_update);
+      ch.on_error(function (err, exc) {
+         if (console && console.log)
+            console.log(err, exc);
+      });
       ch.start();
+      return ch;
    }
 
 
-   function loadMore() {
-      $Couch.view("channel", {
-         startkey: next_startkey,
-         endkey: [current_channel, 0],
-         include_docs: true,
-         limit: 100,
-         descending: true
-      }).done(cb_GotLast100);
-   }
 
-
-   var start = $Utils.getQueryVariable("date");
-
-   if (start) {
-      start = new Date(start).getTime() / 1000;
-      var end = start + 24 * 60 * 60;
-      $Couch.view("channel", {
-         startkey: [current_channel, end],
-         endkey: [current_channel, start],
-         include_docs: true,
-         descending: true
-      }).done(cb_GotHistory);
+   var date = $Utils.getQueryVariable("date");
+   if (date) {
+      loadFullDay(date);
    } else {
-      // get last 100 documents (and show 99)
-      $Couch.view("channel", {
-         startkey: [current_channel, {}],
-         endkey: [current_channel, 0],
-         include_docs: true,
-         limit: 100,
-         descending: true
-      }).done(function (data) {
-         cb_GotLast100(data);
-         // a trick to stop the loader spinning
+      $('#next_page').hide();
+      loadPrevPage().done(function (data) {
+         // setTimeout trick is to stop the browser loader spinning
          window.setTimeout(startUpdates, 1000, data.update_seq);
       });
    }
