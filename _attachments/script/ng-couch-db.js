75ce044cc9b572157737d459f2f67e88f4a0f0f2
Damjan Georgievski <gdamjan@gmail.com>
2014-04-05 19:26:46 +0200

    remove depreceated irclog-couch.js move needed stuff in ng-couch-db.js

---
+++ _attachments/script/ng-couch-db.js
@@ -0,0 +1,35 @@
+// Copyright: 2014, Damjan Georgievski
+// MIT License applies
+//
+'use strict';
+
+angular.module('CouchDB')
+.factory('couchDB', function($http) {
+
+   function CouchDB(url) {
+      // normalize url
+      // replace zero or more trailing slashes with a single one
+      this.url  = url.replace(/\/*$/, '/');
+   }
+
+   CouchDB.prototype.getInfo = function () {
+      var db = this;
+      return $http ({
+         method:  "GET",
+         url:     this.url
+      }).then(function(result) {
+         db.info = result.data;
+      });
+   };
+
+   CouchDB.prototype.getDoc = function(id) {
+      return $http ({
+         method: "GET",
+         url: this.url + id
+      }).then(function(result) {
+         return result.data;
+      });
+   };
+
+   return function (url) { return new CouchDB(url) }
+})
