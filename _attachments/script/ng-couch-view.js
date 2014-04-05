bede155803f34f7dc94348e0192d320b99c7710b
Damjan Georgievski <gdamjan@gmail.com>
2014-04-05 18:04:17 +0200

    create params for the next and previous pager in the view
    Crate those params when the view data is loaded, in the mean time
    next/previous pager will have a promise for their params, so their get
    will wait

--- _attachments/script/ng-couch-view.js
+++ _attachments/script/ng-couch-view.js
@@ -10,42 +10,47 @@ angular.module('CouchDB')
       this.method = method || 'GET';
       this.url    = url;
       this.params = params;
-      this._startkey = params.startkey;
-      this._endkey   = params.endkey;
-      this._firstKey = $q.defer();
-      this._lastKey  = $q.defer();
+      this.nextparams = $q.defer();
+      this.prevparams = $q.defer();
    };
 
    CouchView.prototype.get = function() {
       var self = this;
-      return $q.all([self._startkey, self._endkey]).then(function (keys) {
-         var params = angular.copy(self.params)
-         params.startkey = keys[0];
-         params.endkey   = keys[1];
+      return $q.when(self.params).then(function (params) {
          return getView(self.method, self.url, params).then(function(result) {
-            // pop the extra last row for pagination
+            // prepare params for loadAfter and loadBefore
+            var nextparams = angular.copy(params);
+            var prevparams = angular.copy(params);
             if (result.config.params && result.config.params.limit) {
                if (result.data.rows.length == result.config.params.limit) {
-                  self._lastKey.resolve(result.data.rows.pop().id);
+                  // pop the extra last row for pagination
+                  var last = result.data.rows.pop();
+                  nextparams.startkey = last.key;
+                  nextparams.startkey_docid = last.id;
                }
             }
             if (result.data.rows.length > 0) {
-               self._firstKey.resolve(result.data.rows[0].id);
+               var first = result.data.rows[0];
+               prevparams.endkey = first.key;
+               prevparams.endkey_docid = first.id;
             }
+            self.nextparams.resolve(nextparams);
+            self.prevparams.resolve(prevparams);
             return {rows: result.data.rows, last_seq: result.data.update_seq }
          });
       });
    }
 
    CouchView.prototype.loadAfter = function() {
-      var next = new CouchView(this.url, this.params, this.method);
-      next._startkey = this._lastKey;
-      next._endkey   = this._endkey;
+      var next = new CouchView(this.url, null, this.method);
+      next.params = this.nextparams.promise;
       return next;
    };
 
    CouchView.prototype.loadBefore = function() {
-
+      var prev = new CouchView(this.url, null, this.method);
+      prev.params = this.prevparams.promise;
+      return prev;
    };
 
    function getView (method, url, _params) {
