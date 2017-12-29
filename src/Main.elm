2cf9275eda184d208b78173c448b2c3bf21f5ab0
Дамјан Георгиевски <gdamjan@gmail.com>
2017-12-29 18:24:17 +0100

    Main.elm cleanups :
    * remove the use of Task.perform identity
      ref: http://faq.elm-community.org/#how-do-i-generate-a-new-message-as-a-command
    
    * move `main = ` to the end, it's standard boilerplate

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
-import Html exposing (program, Html)
-import Task
+import Html exposing (program)
 import Time
 import Date
+
 
 import Views
 import Couch
@@ -11,21 +11,11 @@ import Models exposing (..)
 import Helpers exposing (delay)
 
 
-main : Program Never Model Msg
-main =
-    program {
-        init = init "lugola",
-        update = update,
-        view = Views.displayChannelLog,
-        subscriptions = \_ -> Sub.none
-    }
-
-
-init : String -> (Model, Cmd Msg)
+init : String -> ( Model, Cmd Msg )
 init channel =
-    ( Model channel [] "",
-      Task.perform identity (Task.succeed DoInitialView)
-    )
+    let model = Model channel [] ""
+    in
+        update DoInitialView model
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -51,21 +41,31 @@ update msg model =
       _ ->
           (model, Cmd.none)
 
+
+onChannelViewResult : Model -> ViewResult -> (Model, Cmd Msg)
 onChannelViewResult model viewResult =
     let messages = List.reverse viewResult.rows
         last_seq = viewResult.update_seq
+        newmodel = {model | messages = messages, last_seq = last_seq}
     in
-      (
-        { model | messages = messages, last_seq = last_seq },
-        Task.perform identity (Task.succeed DoChanges)
-      )
+        update DoChanges newmodel
 
+
+onChannelChangesResult : Model -> ChangesResult -> (Model, Cmd Msg)
 onChannelChangesResult model changesResult =
     let results = List.sortBy (\doc -> Date.toTime doc.timestamp) changesResult.results
         messages = List.append model.messages results
         last_seq = changesResult.last_seq
+        newmodel = { model | messages = messages, last_seq = last_seq }
     in
-      (
-        { model | messages = messages, last_seq = last_seq },
-        Task.perform identity (Task.succeed DoChanges)
-      )
+        update DoChanges newmodel
+
+
+main : Program Never Model Msg
+main =
+    program {
+        init = init "ubuntu-mk",
+        update = update,
+        view = Views.displayChannelLog,
+        subscriptions = \_ -> Sub.none
+    }
