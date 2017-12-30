5fd3bac4413730020bc9709ae808979e29f7fa3e
Дамјан Георгиевски <gdamjan@gmail.com>
2017-12-31 00:34:28 +0100

    backward compatible routes #/channel
    also catch #/channel/datetime routes where datetime can either be the
    iso-8601 format, or seconds from unix epoch
    
    These will be the same time (when running in CET*)
    /channel/1514676375
    /channel/2017-12-30
    /channel/2017-12-30T01:00:00
    
    * parsing time depends on the local time zone of the browser, which is
      not bad actually, it's what a user would expect

--- src/Routing.elm
+++ src/Routing.elm
@@ -3,14 +3,26 @@ module Routing exposing (..)
 import Navigation exposing (Location)
 import Models exposing (Route(..))
 import UrlParser exposing (..)
+import Date
 
+
+datetime : Parser (Date.Date -> b) b
+datetime =
+    custom "DATETIME" (\str ->
+        case Date.fromString str of
+            (Ok d) -> (Ok d)
+            (Err _) ->
+                String.toFloat str
+                |> Result.map ((*) 1000)
+                |> Result.map Date.fromTime
+    )
 
 matchers : Parser (Route -> a) a
 matchers =
     oneOf
         [ map HomeRoute top,
-          map ChannelRoute string
-        --  map ChannelDateTimeRoute (string </> int )
+          map ChannelRoute (top </> string),
+          map ChannelDateTimeRoute (top </> string </> datetime )
         --  map SettingsRoute "settings"
         --  map SearchRoute "search"
         ]
