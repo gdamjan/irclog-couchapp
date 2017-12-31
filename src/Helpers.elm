4cbe89fa0e8bfb616873f18ab187102ed9fe3298
Дамјан Георгиевски <gdamjan@gmail.com>
2017-12-31 21:20:39 +0100

    add groupWith to Helpers
    Group a list of items by a key.
    Supply an indexing function (eg. .id) and a list of items. groupWith returns a List of (key, sublist) tuples.
    The sublists will retain the order of the original list.

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -3,7 +3,8 @@ module Helpers exposing (.. )
 import Http exposing (encodeUri)
 import Process
 import Task
-import Time
+import Time exposing (Time)
+import Dict
 
 url : String -> List (String,String) -> String
 url baseUrl args =
@@ -21,8 +22,23 @@ queryEscape : String -> String
 queryEscape string =
   String.join "+" (String.split "%20" (Http.encodeUri string))
 
-
-delay : Time.Time -> msg -> Cmd msg
+{-
+    Send a message after a delay
+-}
+delay : Time -> msg -> Cmd msg
 delay time msg =
   Process.sleep time
   |> Task.perform (\_ -> msg)
+
+{-
+    Group a list of items by a key.
+    Supply an indexing function (eg. .id) and a list of items. groupWith returns a List of (key, sublist) tuples.
+    The sublists will retain the order of the original list.
+-}
+groupWith : (a -> comparable) -> List a -> List ( comparable, List a )
+groupWith group list =
+    let alwaysCons a v = Just (a :: Maybe.withDefault [] v)
+        makeGroups a = Dict.update (group a) (alwaysCons a)
+    in
+        List.foldr makeGroups Dict.empty list
+        |> Dict.toList
