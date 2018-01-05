059db30bbbfec087b9c8528a9184a98f2d9c4851
Дамјан Георгиевски <gdamjan@gmail.com>
2018-01-05 02:15:06 +0100

    Date.Date helpers
    - dateOf returns YYYY-mm-dd
    - timeOf returns HH:MM:SS
    - dateTimeOf returns YYYY-mm-ddTHH:MM:SS

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -5,6 +5,7 @@ import Process
 import Task
 import Time exposing (Time)
 import Dict
+import Date
 
 url : String -> List (String,String) -> String
 url baseUrl args =
@@ -42,3 +43,41 @@ groupWith group list =
     in
         List.foldr makeGroups Dict.empty list
         |> Dict.toList
+
+month : Date.Date -> Int
+month d =
+    -- tell me that elm is beautiful
+    case Date.month d of
+        Date.Jan -> 1
+        Date.Feb -> 2
+        Date.Mar -> 3
+        Date.Apr -> 4
+        Date.May -> 5
+        Date.Jun -> 6
+        Date.Jul -> 7
+        Date.Aug -> 8
+        Date.Sep -> 9
+        Date.Oct -> 10
+        Date.Nov -> 11
+        Date.Dec -> 12
+
+dateOf : Date.Date -> String
+dateOf d =
+
+        String.join "-"
+        <| [ Date.year d |> toString,
+             month d |> toString |> String.padLeft 2 '0',
+             Date.day d |> toString |> String.padLeft 2 '0'
+        ]
+
+timeOf : Date.Date -> String
+timeOf d =
+    String.join ":" [
+        Date.hour d |> toString |> String.padLeft 2 '0',
+        Date.minute d |> toString |> String.padLeft 2 '0',
+        Date.second d |> toString |> String.padLeft 2 '0'
+    ]
+
+datetimeOf : Date.Date -> String
+datetimeOf d =
+    dateOf d ++ "T" ++ timeOf d
